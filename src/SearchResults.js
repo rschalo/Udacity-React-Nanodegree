d89437e88ec36fe025fccba1b515583b791a3bd1
rschalo <reed0schalo@gmail.com>
2020-04-16 11:05:15 -0700

    style: remove TODOs

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -4,8 +4,6 @@ import Book from './Book';
 class SearchResults extends Component {
   render() {
     const { searchBooks, sortBooks } = this.props;
-    // TODO: fix "cannot read property 'map' of undefined" at line 8
-    // I think this is happening because searchBooks is defined as undefined when empty
 
     return (
       <div className='search-books-results'>
