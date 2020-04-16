29e99505d9e38e7ca9cf4da5a5022056e83538cf
rschalo <reed0schalo@gmail.com>
2020-04-15 22:04:16 -0700

    refactor: move event handling entirely into Search component, which passes book,shelf to App component

--- src/App.js
+++ src/App.js
@@ -7,47 +7,53 @@ import { Route } from 'react-router-dom';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: [],
+    searchBooks: [],
     isLoaded: false
   };
+  // use searchBooks, pass to Search route
 
   componentDidMount() {
     BooksAPI.getAll().then(books => {
       this.setState(() => ({
         books: books,
         isLoaded: true
-      }))
+      }));
     });
   }
+  // callback from search.js
+  searchNewBooks = (query => {
+    if (query.length > 0) {
+      BooksAPI.search(query).then(books => { this.setState({ searchBooks: books });
+       })
+      } else {
+      this.setState({ searchBooks: {} })
+    };
+  });
 
   // Callback from Bookshelf <select sortBooks> happens here
-  sortBooks = (event, book) => {
-    const { books } = this.state;
-    // value the user selected
-    const { value } = event.target;
-    // gets the book we want to update
-    const bookToUpdate = books.filter(b => b.id === book.id)[0];
-    // updates the shelf property with the selected value
-    bookToUpdate.shelf = value;
-    // gets the old book array, and omit the old book value
-    const booksArray = books.filter(b => b.id !== book.id);
-    // push the updated book into the 'new' books array, and update state
-    booksArray.push(bookToUpdate);
-    // updates the state with new books array
-    this.setState({ books: booksArray });
-    // update the database for data persistence
-    BooksAPI.update(bookToUpdate, value);
+  sortBooks = (book, shelf) => {
+    BooksAPI.update(book, shelf);
+    if (shelf === 'none') {
+      this.setState(prevState => ({
+        books: prevState.books.filter(b => b.id !== book.id)
+      }));
+    } else {
+      book.shelf = shelf;
+      this.setState(prevState => ({
+        books: prevState.books.filter(b => b.id !== book.id).concat(book)
+      }));
+    }
+  };
+
+  resetSearch = () => {
+    this.setState({
+      searchBooks: []
+    });
   };
 
   render() {
-    const { books } = this.state;
+    const { books, searchBooks } = this.state;
     // filter out books by their state.book.shelf and pass as props
     const currentlyReading = books.filter(b => b.shelf === 'currentlyReading');
     const wantToRead = books.filter(b => b.shelf === 'wantToRead');
@@ -71,7 +77,11 @@ class BooksApp extends React.Component {
           path='/search'
           render={() => (
             <Search
+              books={books}
+              searchNewBooks={this.searchNewBooks}
               sortBooks={this.sortBooks}
+              resetSearch={this.resetSearch}
+              searchBooks={searchBooks}
             />
           )}
         />
