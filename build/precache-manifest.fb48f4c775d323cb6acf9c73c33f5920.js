816b576e97fb13522e401bbaac870dc1b2cf4193
rschalo <reed0schalo@gmail.com>
2020-04-16 10:55:35 -0700

    build production file

---
+++ build/precache-manifest.fb48f4c775d323cb6acf9c73c33f5920.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c640c9ebcf7e2cf15d09",
+    "url": "/static/js/main.c640c9eb.chunk.js"
+  },
+  {
+    "revision": "22f50db09120d97f08a5",
+    "url": "/static/js/1.22f50db0.chunk.js"
+  },
+  {
+    "revision": "c640c9ebcf7e2cf15d09",
+    "url": "/static/css/main.2dfc65ed.chunk.css"
+  },
+  {
+    "revision": "729974e27db7025eafbf17be43bc1c07",
+    "url": "/index.html"
+  }
+];
