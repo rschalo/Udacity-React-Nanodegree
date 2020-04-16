f5bdd1a34ae6747e6875d258351079ba66c9c1b3
rschalo <reed0schalo@gmail.com>
2020-04-15 22:04:46 -0700

    feat: event handling that returns both book and its shelf to update in App.js

--- src/Search.js
+++ src/Search.js
@@ -1,113 +1,63 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import * as BooksAPI from './BooksAPI';
+import SearchResults from './SearchResults';
 
 class Search extends Component {
   state = {
-    query: '',
-    results: []
+    query: ''
   };
-  // used as callback will update the input field live
-  updateQuery = query => {
-    this.setState(() => ({
-      query: query
-    }));
-    if (this.state.query && this.state.query.length > 1) {
-      this.getInfo();
+
+  //TODO: move searchbooks into app.js
+
+  /*
+  lifts searchbooks to app, now pass state with
+  searchbooks back down and pass to a search results component
+  */
+  // TODO: let's rewrite how searchbooks is defined in app.js
+  //searchBooks is receiving an undefined value and catching on .map
+  handleChange = e => {
+    const value = e.target.value;
+    if (value.length > 0) {
+      this.setState({ query: value }, () => {
+        this.props.searchNewBooks(value);
+    });
+    } else {
+      this.setState({ query: ''}, () => {
+        this.props.searchNewBooks(this.state.query)
+      })
     }
-  };
-
-  updateResults = results => {
-    this.setState(() => ({
-      results: results
-    }));
+    
   };
   
-  getInfo = () => {
-    BooksAPI.search(this.state.query).then(books => {
-      this.setState(() => ({
-        results: books
-      }));
-    });
-  };
   
-  clearQuery = () => {
-    this.updateQuery('');
-    this.updateResults([]);
-  };
-
-  componentDidMount() {
-    BooksAPI.getAll().then(books => {
-      this.setState(() => ({
-        books: books,
-        isLoaded: true
-      }))
-    });
-  }
 
   render() {
-    const { query, results } = this.state;
-    console.log(typeof(results));
+    const { query } = this.state;
+    const { searchBooks, sortBooks, books, resetSearch } = this.props;
+    console.log(searchBooks);
 
     return (
       <div className='search-books'>
         <div className='search-books-bar'>
-          <Link className='close-search' to='/'>
-            Close
+          <Link to='/'>
+            <button className='close-search' onClick={resetSearch}>
+              Close
+            </button>
           </Link>
           <div className='search-books-input-wrapper'>
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            {/* update text field as input is given */}
             <input
               type='text'
               placeholder='Search by title or author'
               value={query}
-              onChange={event => this.updateQuery(event.target.value)}
+              onChange={this.handleChange}
             />
           </div>
-          <button className='clear-search' onClick={this.clearQuery}>
-          </button>
         </div>
-        <div className='search-books-results'>
-          <ol className='books-grid'>
-            {results.length > 0 &&
-              results.map(book => (
-                <li key={book.id}>
-                  <div className='book'>
-                    <div className='book-top'>
-                      <div className='book-shelf-changer'>
-                        {/* https://github.com/facebook/react/issues/2803#issuecomment-314426759 for handling a disabled select tag option*/}
-                        {/* Default options, like 'Move...' need to have a value equal to the valid empty state for the select. That way, React can associate the value attribute of the select and option tag to know what should be selected.*/}
-                        <select
-                          defaultValue=''
-                          onChange={event => this.props.sortBooks(event, book)}
-                        >
-                          <option value='' disabled>
-                            Move...
-                          </option>
-                          <option value='currentlyReading'>
-                            Currently Reading
-                          </option>
-                          <option value='wantToRead'>Want to Read</option>
-                          <option value='read'>Read</option>
-                          <option value='none'>None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className='book-title'>{`${book.title}`}</div>
-                    <div className='book-authors'>{`${book.authors}`}</div>
-                  </div>
-                </li>
-              ))}
-          </ol>
-        </div>
+        <SearchResults
+          searchBooks={searchBooks}
+          sortBooks={sortBooks}
+          books={books}
+        />
       </div>
     );
   }
